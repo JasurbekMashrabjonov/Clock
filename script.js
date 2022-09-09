a256fe485a5391ee2153dd30f1e51ccbfebf532c
JasurbekMashrabjonov <bahtiyorjonuglu31@gmail.com>
2022-09-09 11:34:29 +0500

    Clock was created

---
+++ script.js
@@ -0,0 +1,26 @@
+setInterval(setClock, 1000);
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+setClock();
+// h=1;
+// function one(){
+//     a = new Date();
+//     document.getElementById('second').style.transformRotate="1deg";
+//     h = h*1 + 1*1;
+//     setTimeout("one()",1000);
+// }
